9deb6625131b77a5fdb3cdc334d91f169d416804
BaraniPrasaath <im.barani2306@gmail.com>
2026-05-12 18:24:05 +0530

    Exploring the http methods

--- src/app.js
+++ src/app.js
@@ -2,13 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res)=>{
-    res.send("Testing end point test");
+app.post("/user",(req, res)=>{
+    //Created the User
+    res.send("User Created Seccessfully");
 })
 
-app.use("/user", (req, res)=>{
-    res.send("Testing end point user");
+app.get("/user",(req, res)=>{
+    //Getting the data
+    res.send({firstName: "Barani Prasaath", lastName: "D"});
 })
+
+app.put("/user",(req, res)=>{
+    //Update the User
+    res.send("User is Updated");
+})
+
+app.delete("/user",(req, res)=>{
+    //Delete the data
+    res.send("The user is deleted");
+})
+
+
+// app.use("/test", (req, res)=>{
+//     res.send("Testing end point test");
+// })
+
+// app.use("/user", (req, res)=>{
+//     res.send("Testing end point user");
+// })
 
 app.use("/",(req, res)=>{
     res.send("Hello Devs");
