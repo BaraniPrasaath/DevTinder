8059bfb0e442b263c496d0b6f12479e32836d512
BaraniPrasaath <im.barani2306@gmail.com>
2026-05-13 12:51:36 +0530

    Error handling using wildcard

--- src/app.js
+++ src/app.js
@@ -2,37 +2,51 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth } = require("../middlewares/adminAuth");
-const { userAuth } = require("../middlewares/userAuth");
+// const { adminAuth } = require("../middlewares/adminAuth");
+// const { userAuth } = require("../middlewares/userAuth");
 
-app.use("/admin", adminAuth);
+// app.use("/admin", adminAuth);
 
-app.get("/user/login", (req, res) => {
-  res.send("User Logged in successfully");
+// app.get("/user/login", (req, res) => {
+//   res.send("User Logged in successfully");
+// });
+
+// app.get("/user/feed", userAuth, (req, res) => {
+//   res.send("Feed is sent");
+// });
+
+// app.get("/user/porfile", userAuth, (req, res) => {
+//   res.send("User profile is sent");
+// });
+
+// app.get("/admin/getAllData", (req, res) => {
+//   res.send("All data is sent");
+// });
+
+// app.get("/admin/update", (req, res) => {
+//   res.send("User updated");
+// });
+
+// app.get("/admin/deleteData", (req, res) => {
+//   res.send("User is deleted");
+// });
+
+// app.get("/admin/porfile", (req, res) => {
+//   res.send("Admin Profile is sent");
+// });
+
+app.get("/user", (req, res) => {
+  // try {
+  throw new Error("abcd");
+  console.log("Response 1");
+  res.send("Sent user data");
+  // } catch {
+  // res.send("User data have some issues");
+  // }
 });
 
-app.get("/user/feed", userAuth, (req, res) => {
-  res.send("Feed is sent");
-});
-
-app.get("/user/porfile", userAuth, (req, res) => {
-  res.send("User profile is sent");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All data is sent");
-});
-
-app.get("/admin/update", (req, res) => {
-  res.send("User updated");
-});
-
-app.get("/admin/deleteData", (req, res) => {
-  res.send("User is deleted");
-});
-
-app.get("/admin/porfile", (req, res) => {
-  res.send("Admin Profile is sent");
+app.use("/", (err, req, res, next) => {
+  res.send("Something went wrong");
 });
 
 app.listen(3000, () => {
