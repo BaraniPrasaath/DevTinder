8e0a85c006fa128a11d813162fa90082d6e4a7b3
BaraniPrasaath <im.barani2306@gmail.com>
2026-05-13 11:06:20 +0530

    Exploring multiple route handlers separately

--- src/app.js
+++ src/app.js
@@ -2,30 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get(
-  "/user",
-  (req, res, next) => {
-    console.log("Response 1 is Working...");
-    // res.send("Response 1");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Response 2 is Working...");
-    // res.send("Response 2");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Response 3 is Working...");
-    // res.send("Response 3");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Response 4 is Working...");
-    res.send("Response 4");
-    // next();
-  },
-);
+app.get("/user", (req, res, next) => {
+  console.log("Response 2 is Working...");
+  res.send("Response 2");
+  next();
+});
+
+app.get("/user", (req, res, next) => {
+  console.log("Response 1 is Working...");
+  res.send("Response 1");
+  next();
+});
 
 app.listen(3000, () => {
   console.log("The Server is Listerning to the port 3000...");
-});
+})
